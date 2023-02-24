768797b688099a0402e6246cfddeb6c9ff0d6a0e
Reetool16 <reetoolgupta16.com>
2023-02-24 19:06:57 +0530

    refactor

---
+++ index.js
@@ -0,0 +1,18 @@
+console.log("test")
+
+// for (var i = 0; i < 10; i++) {
+//      setTimeout(function () {
+//           console.log(i)
+//      }, 1000)
+// }
+
+// for (var i = 0; i < 10; i++) {
+//      setTimeout(function () {
+//           console.log(i)
+//      }, 1000)
+// }
+
+let string = "hy my name is ritul";
+console.log(string.split('').reverse().join(''));
+console.log(x);
+var x = 2; // always delcaration are hoisted 
